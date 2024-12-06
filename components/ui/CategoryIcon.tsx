7550e610a50588abd8e840b6f373aa3488cf940c
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-12-05 23:47:57 -0500

    add Dinamic Routing with Next

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,4 +1,6 @@
 import { Category } from "@prisma/client"
+import Image from "next/image"
+import Link from "next/link"
 
 
 
@@ -9,6 +11,23 @@ type CategoryIconProps = {
 
 export default function CategoryIcon({ category }: CategoryIconProps) {
     return (
-        <div>{category.name}</div>
+        <div
+            className={` flex items-center gap-4 w-full border-t border-gray-200 p-3
+            last-of-type:border-b`}
+        >
+            <div className=" w-16 h-16  relative">
+                <Image
+                    fill
+                    src={`/icon_${category.slug}.svg`}
+                    alt="CategoryIcon" />
+
+            </div>
+            <Link 
+            href={`/order/${category.slug}`}
+            className=" text-xl font-bold"
+            >
+                {category.name}
+            </Link>
+        </div>
     )
 }
