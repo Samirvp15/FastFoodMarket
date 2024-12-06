e01c722670170428233d71706a38df1006dd5428
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-12-05 23:06:54 -0500

    add data fetching from Next Server with globalPrismaQuery

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,6 +1,15 @@
+import {prisma} from '@/src/lib/prisma'
+
+async function getCategories(){
+  const categories = await prisma.category.findMany()
+  console.log(categories)
+}
 
 
-export default function OrderSidebar() {
+export default async function OrderSidebar() {
+
+  await getCategories()
+
   return (
    <aside className=" md:w-72 md:h-screen bg-white">
     OrderSidebar
