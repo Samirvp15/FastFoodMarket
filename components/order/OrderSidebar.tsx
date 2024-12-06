e8aa8e019e2da3537cbed88d6a24bd7d79bf7d9e
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-12-05 23:28:01 -0500

    add icons and categoriesIcons

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,18 +1,27 @@
-import {prisma} from '@/src/lib/prisma'
+import { prisma } from '@/src/lib/prisma'
+import CategoryIcon from '../ui/CategoryIcon'
 
-async function getCategories(){
-  const categories = await prisma.category.findMany()
-  console.log(categories)
+async function getCategories() {
+  return await prisma.category.findMany()
 }
 
 
 export default async function OrderSidebar() {
 
-  await getCategories()
+  const categories = await getCategories()
 
   return (
-   <aside className=" md:w-72 md:h-screen bg-white">
-    OrderSidebar
-   </aside>
+    <aside className=" md:w-72 md:h-screen bg-white">
+
+      <nav className='mt-10'>
+        {categories.map(category => (
+          <CategoryIcon
+            key={category.id}
+            category={category}
+          />
+        ))}
+      </nav>
+
+    </aside>
   )
 }
---
+++ components/ui/CategoryIcon.tsx
@@ -0,0 +1,14 @@
+import { Category } from "@prisma/client"
+
+
+
+type CategoryIconProps = {
+    category: Category
+}
+
+
+export default function CategoryIcon({ category }: CategoryIconProps) {
+    return (
+        <div>{category.name}</div>
+    )
+}
