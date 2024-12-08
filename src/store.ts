ca00d4af46dc9eeee081e01e9d90726e6290e4a6
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-12-07 22:14:24 -0500

    add orderItems to Order[] with zustand and show them in Cart with OrderProductDetails

--- src/store.ts
+++ src/store.ts
@@ -11,10 +11,18 @@ interface Store {
 }
 
 
-export const useStore = create<Store>(() => ({
+export const useStore = create<Store>((set) => ({
     order: [],
     addToOrder: (product) => {
-        console.log('agregando', product)
+        const {categoryId, image, ...data} = product
+
+        set((state) => ({
+            order: [...state.order, {
+                ...data,
+                quantity: 1,
+                subtotal: 1 * data.price
+            }]
+        }))
     }
 }))
 
