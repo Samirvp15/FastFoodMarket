25762449fd34a2487ba3828c50eb7db1c46d01bc
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-12-07 22:35:17 -0500

    add

--- src/store.ts
+++ src/store.ts
@@ -11,17 +11,30 @@ interface Store {
 }
 
 
-export const useStore = create<Store>((set) => ({
+export const useStore = create<Store>((set, get) => ({
     order: [],
     addToOrder: (product) => {
-        const {categoryId, image, ...data} = product
+        const { categoryId, image, ...data } = product
+        let order: OrderItem[] = []
 
-        set((state) => ({
-            order: [...state.order, {
+        //  PARA EVITAR ITEMS DUPLICADOS
+        if (get().order.find(item => item.id === product.id)) {
+            //INCREMENTAR SU CANTIDAD Y NO DUPLICADOS
+            order = get().order.map(item => item.id === product.id ? {
+                ...item,
+                quantity: item.quantity + 1,
+                subtotal: item.price * (item.quantity + 1)
+            } : item)
+        } else {
+            order = [...get().order, {
                 ...data,
                 quantity: 1,
                 subtotal: 1 * data.price
             }]
+        }
+
+        set(() => ({
+            order: order
         }))
     }
 }))
