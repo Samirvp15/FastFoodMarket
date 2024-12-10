78e8d2882a9909458bbccac2abdc6e1125c94e31
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-12-09 23:45:00 -0500

    add SearchProductForm stylized

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,7 +1,9 @@
+import ProductSearchForm from "@/components/products/ProductSearchForm";
 import ProductsPagination from "@/components/products/ProductsPagination";
 import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import Link from "next/link";
 
 
 async function productCount() {
@@ -35,14 +37,22 @@ export default async function ProductsPage({ searchParams }: { searchParams: { p
   const productsData = await getProducts(page, pageSize)
   const totalProductsData = await productCount()
   const [products, totalProducts] = await Promise.all([productsData, totalProductsData])
-  const totalPages = Math.ceil(totalProducts / pageSize) 
+  const totalPages = Math.ceil(totalProducts / pageSize)
 
 
   return (
     <>
-      <Heading>
-        Administrar Productos
-      </Heading>
+      <Heading>Administrar Productos</Heading>
+
+      <div className=" flex flex-col lg:flex-row lg:justify-between gap-5">
+        <Link
+          href={`/admin/products/new`}
+          className=" bg-amber-400 w-full lg:w-auto text-lg px-10 py-3
+            text-center font-bold cursor-pointer"
+        >Crear producto</Link>
+
+        <ProductSearchForm />
+      </div>
 
       <ProductTable
         products={products}
---
+++ components/products/ProductSearchForm.tsx
@@ -0,0 +1,22 @@
+
+
+import React from 'react'
+
+export default function ProductSearchForm() {
+    return (
+        <form className=' flex items-center'>
+            <input
+                type="text"
+                placeholder='Buscar Producto'
+                className=' p-2 placeholder-gray-400 w-full'
+                name='search'
+            />
+
+            <input
+                type="submit"
+                className=' bg-indigo-600 p-2 uppercase text-white cursor-pointer'
+                value="Buscar" />
+
+        </form>
+    )
+}
